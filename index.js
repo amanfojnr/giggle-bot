4cfb8df60c555f982719a519e07b107d8ee7f3dc
amanfojnr <kenmarch@zoho.com>
2017-07-11 15:26:22 +0000

    aligned code

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const Bot = require('./bot.js');
        SLACK_TOKEN = <BOT_SLACK_TOKEN_HERE> node index.js
 */
 
-const token = process.env.SLACK_TOKEN; 
+const token = process.env.SLACK_TOKEN;
 
 // create bot instance
 const bot = new Bot({
-                     token: token,
-                     autoReconnect: true,
-                     autoMark: true
-                     });
+    token: token,
+    autoReconnect: true,
+    autoMark: true
+});
 
 /* 
 bot respond commands
@@ -25,95 +25,92 @@ specific string patterns if found giggle-bot responds based on
 particular pattern
 */
 
-bot.respondTo('giggle help', (message, channel, user) => { 
+bot.respondTo('giggle help', (message, channel, user) => {
         bot.send(' - To make me crack a random joke type \`joke\`\n\
         - To make me joke about a friend or anyone, type \`joke <username>\` ', channel);
     },
-     true);
+    true);
 
 bot.respondTo('joke', (message, channel, user) => {
 
-    let tokenArr = message.text.split(' ');
+        let tokenArr = message.text.split(' ');
 
-    if (tokenArr.length === 1) {
-        
-        /* 
-        making giggle bot send joke to channel if no argument is specified
+        if (tokenArr.length === 1) {
 
-        eg.
-        bot.send('Sorry, somebody forgot to put some jokes in me, ok', channel);
+            /* 
+            making giggle bot send joke to channel if no argument is specified
 
-        write send message code here
-        */
-        let url = 'https://api.chucknorris.io/jokes/random';
+            eg.
+            bot.send('Sorry, somebody forgot to put some jokes in me, ok', channel);
 
-fetch(url)
-    .then(function(res) {
-        return res.json();
-    }).then(function(json) {
-       bot.send(json.value,channel);
-    });
-    }
+            write send message code here
+            */
+            let url = 'https://api.chucknorris.io/jokes/random';
 
-    else {
-      /* 
-      taking argument and cracking a joke with the given argument
+            fetch(url)
+                .then(function(res) {
+                    return res.json();
+                }).then(function(json) {
+                    bot.send(json.value, channel);
+                });
+        } else {
+            /* 
+            taking argument and cracking a joke with the given argument
 
-      eg. 
-      let args = tokenArr.slice(1);
-      bot.send(`${args[0]} looks like ..., arrhh I need some damn jokes`, channel);
+            eg. 
+            let args = tokenArr.slice(1);
+            bot.send(`${args[0]} looks like ..., arrhh I need some damn jokes`, channel);
 
-      write send message code here
-      */ 
-       let url = 'https://api.chucknorris.io/jokes/random';
-       let args = tokenArr.slice(1);
-       fetch(url)
-    .then(function(res) {
-        return res.json();
-    }).then(function(json) {
-       
-       
-       let joke = json.value.replace(/(chuck norris|chuck|chucky)/gi ,args[0]);
-      
-       bot.send(joke, channel);
-    });
-      
-    }
-    
+            write send message code here
+            */
+            let url = 'https://api.chucknorris.io/jokes/random';
+            let args = tokenArr.slice(1);
+            fetch(url)
+                .then(function(res) {
+                    return res.json();
+                }).then(function(json) {
+
+
+                    let joke = json.value.replace(/(chuck norris|chuck|chucky)/gi, args[0]);
+
+                    bot.send(joke, channel);
+                });
+
+        }
+
     },
     true);
 
 bot.respondTo('mom', (message, channel, user) => {
-    
+
     let tokenArr = message.text.split(' ');
     if (tokenArr.length === 1) {
-            let url = 'http://api.yomomma.info/';
+        let url = 'http://api.yomomma.info/';
 
-function getJoke(){
-     fetch(url)
-    .then(function(res) {
-        return res.json();
-    }).then(function(json) {
-        bot.send(json.joke,channel);
-    });}
-    setTimeout(getJoke,2000);
-    }
-    else{
-        bot.send("It's not nice to target a friend's MOMMA!!!! Even Jokester has his limits!!",channel);
+        function getJoke() {
+            fetch(url)
+                .then(function(res) {
+                    return res.json();
+                }).then(function(json) {
+                    bot.send(json.joke, channel);
+                });
+        }
+        setTimeout(getJoke, 2000);
+    } else {
+        bot.send("It's not nice to target a friend's MOMMA!!!! Even Jokester has his limits!!", channel);
     }
 }, true);
 
 bot.respondTo('gigpic', (message, channel, user) => {
-    
+
     let tokenArr = message.text.split(' ');
     if (tokenArr.length === 1) {
-   function selfie(){
-   bot.send("http://www.scenictrail.com/LonghornSheep_2.jpg",channel);
-    }
-    setTimeout(selfie,2000);
-    }
-    else{
-        bot.send("huh",channel);
+        function selfie() {
+            bot.send("http://www.scenictrail.com/LonghornSheep_2.jpg", channel);
+        }
+        setTimeout(selfie, 2000);
+    } else {
+        bot.send("huh", channel);
     }
 }, true);
 
